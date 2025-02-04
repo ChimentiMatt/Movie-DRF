74fa4a642981f5aa6c7db8e1a70635db908ed925
chimentimatt <chimcode@gmail.com>
2025-02-04 12:48:11 -0800

    removed comment

--- movieproject/settings.py
+++ movieproject/settings.py
@@ -6,11 +6,6 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key')

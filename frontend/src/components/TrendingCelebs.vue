dbe1b42fbb388d33ff922b31487e5633ce31204a
chimentimatt <chimcode@gmail.com>
2025-02-13 12:52:12 -0800

    alignment fixes for Actors in theatres

--- frontend/src/components/TrendingCelebs.vue
+++ frontend/src/components/TrendingCelebs.vue
@@ -1,32 +1,35 @@
 <template>
-  <div class="mt-[2.5rem] text-black dark:text-[#F5F5F5]">
+  <div class="flex flex-col lg:flex-row justify-center mt-[2.5rem] text-black dark:text-[#F5F5F5]">
 
-    <h1 class="font-semibold text-2xl ">Actors in theatres</h1>
+    <div class="lg:w-[290px]">
 
-    <div class="flex flex-col gap-2 items-center justify-evenly pt-2">
-      <div v-for="movie in filteredMovies" class="">
-        <p class="text-center">
-          {{ movie.title }}
-        </p>
-        <div class="flex justify-end gap-2 text-[0.5rem]">
-          Page {{ Math.floor(currentMovieSet) + 1 }} of {{ Math.ceil(this.movies.length) }}
-          <button v-if="this.currentMovieSet - 1 >= 0" @click="previousMovies()">Previous</button>
-          <button v-if="this.currentMovieSet + 1 < this.movies.length" @click="nextMovies()">Next</button>
-        </div>
-        <div class="flex flex-col lg:flex-row mt-2">
-
-          <div v-for="person in movie.people" class="flex flex-col justify-center items-center w-[15rem]">
-
-            <router-link :to="'/person/' + 'Tim Allen'">
-              <img :src="`https://image.tmdb.org/t/p/w500/${person.headshot_url}`" alt="Robin Williams"
-                class="rounded-full h-[10rem] w-[10rem] object-cover">
-            </router-link>
-            <div class="flex flex-col items-center justify-center">
-              <p>
-                {{ person.name }}
-              </p>
+      <h1 class="font-semibold text-2xl text-center">Actors in Theatres</h1>
+      
+      <div class="flex flex-col gap-2 items-center justify-evenly pt-2">
+        <div v-for="movie in filteredMovies" class="">
+          <p class="text-center">
+            {{ movie.title }}
+          </p>
+          <div class="flex justify-end gap-2 text-[0.5rem]">
+            Page {{ Math.floor(currentMovieSet) + 1 }} of {{ Math.ceil(this.movies.length) }}
+            <button v-if="this.currentMovieSet - 1 >= 0" @click="previousMovies()">Previous</button>
+            <button v-if="this.currentMovieSet + 1 < this.movies.length" @click="nextMovies()">Next</button>
+          </div>
+          <div class="flex flex-col lg:flex-row mt-2">
+            
+            <div v-for="person in movie.people" class="flex flex-col justify-center items-center w-[15rem]">
+              
+              <router-link :to="'/person/' + 'Tim Allen'">
+                <img :src="`https://image.tmdb.org/t/p/w500/${person.headshot_url}`" alt="Robin Williams"
+                class="rounded h-[10rem] w-[10rem] object-cover">
+              </router-link>
+              <div class="flex flex-col items-center justify-center">
+                <p>
+                  {{ person.name }}
+                </p>
+              </div>
+              
             </div>
-
           </div>
         </div>
       </div>

0320e2d793932013a092084fcef632d1f6d6aac0
chimentimatt <chimcode@gmail.com>
2025-02-13 12:43:09 -0800

    carousel fix

--- frontend/src/components/Carousel.vue
+++ frontend/src/components/Carousel.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="flex flex-col">
-
     <h1 class="mb-2 lg:ml-[2.5rem] text-center lg:text-left text-2xl font-semibold">In Theatres</h1>
 
     <div class="relative overflow-hidden" @mouseenter="pauseCarousel" @mouseleave="startCarousel">
       <div class="flex lg:w-[290px] transition-transform duration-500 ease-in-out h-full"
         :style="{ transform: `translateX(-${currentIndex * 100}%)` }">
         <div v-for="(movie, index) in movies" :key="index"
-          class="flex justify-center items-center flex-shrink-0 w-full relative  overflow-hidden">
-
+          class="flex justify-center items-center flex-shrink-0 w-full relative overflow-hidden">
           <router-link :to="'/movie/' + movie.id">
             <img :src="movie.poster_url" :alt="movie.title"
-              class="h-[315px] w-[210px] rounded rounded-lg object-cover object-contain hover:opacity-60"/>
+              class="h-[315px] w-[210px] rounded-lg object-cover object-contain hover:opacity-60" />
           </router-link>
-      
         </div>
       </div>
 
@@ -37,14 +34,14 @@
     </div>
 
     <!-- Movie Title -->
-    <div v-for="item in moviesIndex" :key="item.index" class="flex justify-center mt-1">
-      <h1 v-if="item.index === currentIndex" class="absolute font-semibold">{{ item.title }}</h1>
+    <div class="flex justify-center mt-1">
+      <h1 v-if="movies.length > 0" class="absolute font-semibold">{{ movies[currentIndex]?.title }}</h1>
     </div>
   </div>
 </template>
 
 <script>
-import { ref, computed, onMounted } from "vue";
+import { ref, computed, onMounted, onUnmounted } from "vue";
 import { useInTheatresStore } from "@/stores/inTheatres.js";
 
 export default {
@@ -53,34 +50,53 @@ export default {
     const currentIndex = ref(0);
     const intervalId = ref(null);
 
-    // Reactive computed properties
     const movies = computed(() => inTheatresStore.movies);
-    const moviesIndex = computed(() =>
-      inTheatresStore.movies.map((movie, index) => ({
-        index,
-        title: movie.title,
-      }))
-    );
 
-    // Fetch movies when component is mounted
     onMounted(async () => {
       await inTheatresStore.fetchMovies();
       startCarousel();
     });
 
+    onUnmounted(() => {
+      pauseCarousel();
+    });
+
+    function nextSlide() {
+      currentIndex.value = (currentIndex.value + 1) % movies.value.length;
+    }
+
+    function prevSlide() {
+      currentIndex.value = (currentIndex.value - 1 + movies.value.length) % movies.value.length;
+    }
+
+    function goToSlide(index) {
+      currentIndex.value = index;
+      pauseCarousel();
+      startCarousel();
+    }
+
     function startCarousel() {
+      if (intervalId.value) clearInterval(intervalId.value);
       intervalId.value = setInterval(() => {
-        currentIndex.value =
-          (currentIndex.value + 1) % moviesIndex.value.length;
+        currentIndex.value = (currentIndex.value + 1) % movies.value.length;
       }, 3000);
     }
 
+    function pauseCarousel() {
+      if (intervalId.value) {
+        clearInterval(intervalId.value);
+        intervalId.value = null;
+      }
+    }
+
     return {
-      inTheatresStore,
       movies,
-      moviesIndex,
       currentIndex,
+      nextSlide,
+      prevSlide,
+      goToSlide,
       startCarousel,
+      pauseCarousel,
     };
   },
 };
